ad1fe53e48a1dc9b8e77eee79100f5095ca7fbfe
Alek Evans <github@alekeagle.com>
2023-12-20 18:13:04 -0900

    Very Professional Video Games Incorporated

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <InitialAnimation ref="initAnim" />
+</template>
+
+<script lang="ts" setup>
+  import { ref, onMounted } from 'vue';
+  import InitialAnimation from '@/components/InitialAnimation.vue';
+
+  const initAnim = ref<typeof InitialAnimation>();
+
+  onMounted(async () => {
+    await initAnim.value?.begin();
+  });
+</script>
---
+++ src/components/InitialAnimation.vue
@@ -0,0 +1,41 @@
+<template>
+  <TopToBottomSlide ref="topToBottomSlide" :duration="4000">
+    <img :src="professionalBanner" alt="Professional Banner" />
+    <h1>Very Professional Video Games Incorporated ™©®</h1>
+  </TopToBottomSlide>
+</template>
+
+<script lang="ts" setup>
+  import TopToBottomSlide from './TopToBottomSlide.vue';
+  import professionalBanner from '@/assets/images/lol.jpg';
+  import { ref } from 'vue';
+
+  let topToBottomSlide = ref<typeof TopToBottomSlide>();
+
+  async function begin() {
+    await topToBottomSlide.value?.begin();
+  }
+
+  defineExpose({ begin });
+</script>
+
+<style scoped>
+  img {
+    aspect-ratio: 16/9;
+    width: 75vw;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  div > h1 {
+    /* Scale the font size to match the width of the image regardless of the screen size */
+    font-size: calc(75vw / 25.5);
+    font-weight: 900;
+    color: #ff0000;
+    text-shadow: 0 0 10px #ff0000;
+  }
+</style>
